ea1614473b613fe2208ab1a49b4bd913b3a4221d
davidfrancis30 <davidfrancisjune30@gmail.com>
2023-02-21 12:53:39 +0100

    task completed

---
+++ git/index.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 1; i++) {
+	let str = "";
+	for (let j = 1; j <=5 ; j++) {
+        str += "*";
+        console.log(str);
+	}
+console.log('-------');
+}
+
+
+// for (let i = 0; i < 1; i++) {
+// 	let str = "";
+// 	for (let j = 1; j <= 5; j++) {
+// 		str += "*";
+// 		console.log(str);
+// 	}
+// 	console.log("-------");
+// }
